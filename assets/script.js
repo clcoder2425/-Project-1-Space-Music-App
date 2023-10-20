b88ea682c9cc2ad873c74e4654626bf518c9dfa7
Brian King <Thebeakeh@gmail.com>
2023-10-19 21:14:22 -0500

    Added controls to Youtube video playing

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,12 @@
-
-console.log(searchFieldEl)
-
-
+// api_key=0kRnAVYNc2gsCR3nOYw7LjB2uBvKsB75RLIkT25q
 var searchFieldEl = document.querySelector('#searchField');
 var searchFieldInput = document.querySelector('#searchFieldInput');
 var displayEl = document.querySelector("#display")
-
+var muteButton= document.querySelector('#muteBtn')
+var playPauseButton= document.querySelector('#playPause')
+var restartMusicButton= document.querySelector('#restartButton')
+var volumeUpBtn= document.querySelector('#volumeUp')
+var volumeDwnBtn= document.querySelector('#volumeDown')
 
 var formSubmitHandler = function (event) {             // when clicked, this is run
   event.preventDefault();
@@ -14,7 +15,6 @@ console.log(usersSearchInput)
 getSearch(usersSearchInput)                         // runs the search function with users search input
 play()
 };
-
 
 var getSearch = function (usersSearchInput) {
 // Useable Api Urls
@@ -30,27 +30,22 @@ function displayPhotos(imgURL){
   img.title = cameraName;
   // document.body.appendChild(img);        //  adds it to the <body> tag
   displayEl.appendChild(img);                    
- 
 }
 }
 
-fetch(apiUrl)                                         // FETCH
+fetch(apiUrl)                                         // FETCH Request
 .then(function (response) {
   if (response.ok) {
     console.log(response);
     response.json().then(function (data) {
       console.log(data);
-
       if (data.photos.length === 0) {
         displayEl.textContent = 'No Photos found. Please select another date';
         return;
       }
 
-
-      
-
-      displayEl.innerHTML=''
-      for (var i = 0; i < data.photos.length; i++) {          // for the length of the string returned, go through each one
+  displayEl.innerHTML=''                                        // Clears Images when a search is made to prepare for a new image.
+  for (var i = 0; i < data.photos.length; i++) {          // for the length of the string returned, go through each one
         imgURL=(data.photos[i].img_src);                       // set the image source to imgURL
         cameraName=(data.photos[i].camera.full_name);          // Set the name of the Camera that took the photo to cameraName
         displayPhotos(imgURL);
@@ -64,30 +59,8 @@ fetch(apiUrl)                                         // FETCH
 });
 };
 
-
-searchFieldEl.addEventListener('submit', formSubmitHandler);
-
-
-
-
-// var player;
-// function onYouTubeIframeAPIReady() {
-//   player = new YT.Player('player', {
-//     height: '390',
-//     width: '640',
-//     videoId: '8jPL_5BoKuA',
-//     playerVars: {
-//       'playsinline': 1
-//     },
-//     events: {
-//       'onReady': onPlayerReady,
-//       'onStateChange': onPlayerStateChange
-//     }
-//   });
-// }
-
- // 2. This code loads the IFrame Player API code asynchronously.
- var tag = document.createElement('script');
+//----------------------------------------------------------------------------------------------------
+ var tag = document.createElement('script'); //  loads the IFrame Player API code asynchronously.
  tag.src = "https://www.youtube.com/iframe_api";
  var firstScriptTag = document.getElementsByTagName('script')[0];
  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
@@ -103,29 +76,60 @@ searchFieldEl.addEventListener('submit', formSubmitHandler);
        'playsinline': 1,
      },
      events: {
-    
      }
-   });
- }
+   })};
  
 
-
+// Youtube functions
  function stopVideo() {
    player.stopVideo();
- }
+ };
 
 function play(){
-player.seekTo(0,false)    //sets the player to 0 second mark
+  player.seekTo(1,true)    //sets the player to 0 second mark
 player.setVolume(2)
 player.playVideo()
-}
+};
 
 function muteMusic(){
-player.mute()
-}
+if (player.isMuted()){
+  muteButton.innerHTML="Mute"
+  player.unMute()
+}else{player.mute()
+muteButton.innerHTML="Unmute"
+}};
+
+function playPause(){
+if (player.getPlayerState()!=1){          // if player isnt playing, play. Otherwise pause video
+  player.playVideo()
+  playPauseButton.innerHTML="Pause"
+}else{
+    player.pauseVideo()
+ playPauseButton.innerHTML="Play"
+}};
+
+function restartMusic(){
+  player.seekTo(1,true)    //sets the player to 0 second mark
+  player.setVolume(2)
+  player.playVideo()
+};
+
+function volumeUp(){
+  var volume = player.getVolume()
+  player.setVolume(volume+1)
+};
+
+function volumeDown(){
+  var volume = player.getVolume()
+  player.setVolume(volume-1)
+};
 
 
 
-muteButton.addEventListener('click', muteMusic);
-
+volumeUpBtn.addEventListener('click',volumeUp );                        //Event Listeners
+volumeDwnBtn.addEventListener('click', volumeDown);
+restartMusicButton.addEventListener('click', restartMusic);
+playPauseButton.addEventListener('click', playPause);                 // when playpauseButton is clicked, runs playPause Function
+muteButton.addEventListener('click', muteMusic);                    // Mutes/unmutes when Mute button is clicked
+searchFieldEl.addEventListener('submit', formSubmitHandler);
 
